05a0f9e362a8dcf170d6ca32ff2e32b7ee005040
sheng kiat <sheng kiat@DESKTOP-ESER2VA>
2016-05-14 12:24:25 +0800

    Add Username Validator

---
+++ Active_Learning_Service_Group4_Solution/ActiveLearning.ServiceInterfaces/ServiceAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+
+namespace ActiveLearning.ServiceInterfaces
+{
+    public class ServiceAuthenticator: System.IdentityModel.Selectors.UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null || password == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (!(userName == "fayaz" && password == "soomro"))
+            {
+                throw new SecurityTokenException("Incorrect Username or Password");
+            }
+        }
+    }
+}
