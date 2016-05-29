ecdf90ca854937cc3d641167e50b534accda5ce4
chenghao1983 <joechenghao@gmail.com>
2016-05-29 22:49:10 +0800

    updated service

--- Active_Learning_Service_Group4_Solution/ActiveLearning.WF/Activity/UploadCourseContentActivity.cs
+++ Active_Learning_Service_Group4_Solution/ActiveLearning.WF/Activity/UploadCourseContentActivity.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Activities;
+﻿using System.Activities;
 using System.Web;
 using ActiveLearning.Business.Implementation;
-using ActiveLearning.Common;
 
 namespace ActiveLearning.WF.Activity
 {
